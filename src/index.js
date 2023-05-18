f55015187ff3fcf5f6868b89d351709aee87bdba
Ben Aitcheson <benaitcheson@hotmail.com>
2023-05-18 22:08:28 +1000

    ADD: material ui theme

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from './App';
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { theme } from "./theme";
+
+<React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <App />
+    <CssBaseline />
+  </ThemeProvider>
+</React.StrictMode>
